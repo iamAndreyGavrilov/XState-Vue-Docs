493fbdfa0a892d9f340237f7b65fc31f8b817145
iamAndreyGavrilov <79842149+iamAndreyGavrilov@users.noreply.github.com>
2022-05-07 19:16:04 +0500

    123

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
-  <div><h1>Hello</h1></div>
+  <div>
+    <p>123sdfdsf</p>
+    <button @click="send('Unmute')">{{ toggleButtonLabel }}</button>
+    <button>Hide video</button>
+  </div>
 </template>
 
 <script>
+import { computed } from "@vue/runtime-core";
+
 import { useMachine } from "@xstate/vue";
+// import promiseMachine from "./dogMachine";
+// import machine from "./dogMachine";
+import machine2 from "./dogMachine";
+
 export default {
   name: "App",
   components: {},
+  setup() {
+    const { state } = useMachine(machine2);
+    const toggleButtonLabel = computed(() => {
+      const current = state.value;
+      if (current.matches("Unmuted")) {
+        return "Unmuted";
+      } else if (current.matches("Muted")) {
+        return "Muted";
+      }
+    });
+    return {
+      toggleButtonLabel,
+    };
+  },
 };
 </script>
 
--- src/dogMachine.js
+++ src/dogMachine.js
@@ -1,49 +1,49 @@
 import { createMachine } from "xstate";
 
 const promiseMachine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QBED2UB0BBA7gQwGswMBlAGzDAAcBPAYgBlUBXCAAgDtUBLAYzESgqqWNwAu3VB0EgAHogBMAFgCcGAOwAOBZoAMmgGwBWFUoDM6swYA0IGogC0Cs0owmFRgwpUGDSgyoK6gC+wbZomLiExABCYFBQ3BxQdADi3ABuYGwAKgBOYHhiMsKiElIy8ggqmhoAjJqeut4qRpZmtvYIDhZqZnUeSuoGdQ1KdUqh4ejY+ER0AGJ4ZGSwbHiwFNQlIuKS0khyjv6uZjrauiqmWpoddo4j6hguvUqNmoYGIWEgEdiblCojBY7AAcjx+DsyvtKo4jAoMNclLoUdc6j4FDZ7t1NOYMOYjGZ4UZ-Fc2lNfjMsADqHQSABbMArNYAM1QqAgUL2FUOVQcdTMug0ukJwzqBm0DQMdy6DnUrURIysfg+RmaBlCPy4EDgMj+USIpC2tC55QOoCqZkFbl08t8Rk8bSCSk6jiJZgwmhULiUhJJSkxKgp+rmxBIvDwBU5h1K3PNR26bU93l9ChRul9ZyULux-IUdUR-ltJIFbTMjWDVNDGDiCSSUFNMN5iFuGExzl0DWUtsFRld3UFT1tpgsFjezlxlciocbPItjk0Be0pnh6czyn7DlTzwG0pUl29w8mP31NKos-jfPHGHFKN8WgMtp7m9xam9ft85n6EqnF9hid0Awb0fQCvkMJ9LE3QIEXfK03gFHxVE1YIgA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QBED2UB0BBA7gQwGswMBlAGzDAAcBPAYgBlUBXCAAgDtUBLAYzESgqqWNwAu3VB0EgAHogBMAFgCcGAOwAOBZoAMmgGwBWFUoDM6swYA0IGogC0Cs0owmFRgwpUGDSgyoK6gC+wbZomLiExABCYFBQ3BxQdADi3ABuYGwAKgBOYHhiMsKiElIy8ggAjNWaGGYuCjrq-ko6+rb2CA4WZhhKdZ5Gmj4GZtUGoeHo2PhEdABieGRksGx4sBTUJSLiktJIco5tDTrauiqmWppmXY4G1eoN5uqmmiOGBiFhIBHYW0oVEYLHYADkePxdmUDpVHEYFBhrkpdKjrtUfAobHZHJpzAMzEZCR5-FcjD8ZpFAdQ6CQALZgVbrABmqFQEGh+wqRyqDmqZl0Gl0RPUjwM2jq43uPTeRiRjysfk0H10WNCvy4EDgMn+USIpG2tE55UOoCqjUFRl0b18RmG6iCSmlvSJGFGLiURKMSnaAWmf1meuIJF4eAKHKOpS5puOPXJbu8noUqN0nrMyidOJ61QU1SR-mt3v55LMH39uvmsXiiWSxthPMQtwwWOcujqymtAqMzoFz2tpj6rW0paU5cDlbr3LNuLz2lMCJTaeUzp9BgGqs01W97sCorHVMNk5jvKU2gwk1Rvi0BmtnedeLUKkJhN85gm4v3R7hcd0a4vv++Qxb0sZ1AkRFQMQddpWkJEZ1WCIA */
   createMachine({
-  id: "Dog",
-  initial: "Awake",
-  states: {
-    Awake: {
-      initial: "Begging",
-      states: {
-        Sleepy: {
-          on: {
-            "Loud noice": {
-              target: "Scared",
+    id: "Dog",
+    initial: "Awake",
+    states: {
+      Awake: {
+        initial: "Begging",
+        states: {
+          Sleepy: {
+            on: {
+              "Loud noice": {
+                target: "Scared",
+              },
+            },
+          },
+          Scared: {},
+          Begging: {
+            on: {
+              "Give Treat": {
+                target: "Begging",
+                internal: false,
+              },
             },
           },
         },
-        Scared: {},
-        Begging: {
-          on: {
-            "Give Treat": {
-              target: "Begging",
-              internal: false,
-            },
+        on: {
+          "Falls asleep": {
+            target: "Asleep",
           },
         },
       },
-      on: {
-        "Falls asleep": {
-          target: "Asleep",
+      Asleep: {
+        on: {
+          "Loud Noice": {
+            target: "#Dog.Awake.Scared",
+          },
+          "Smells food": {
+            target: "#Dog.Awake.Sleepy",
+          },
         },
       },
     },
-    Asleep: {
-      on: {
-        "Loud Noice": {
-          target: "#Dog.Awake.Scared",
-        },
-        "Smells food": {
-          target: "#Dog.Awake.Sleepy",
-        },
-      },
-    },
-  },
-});
+  });
 
 const machine =
   /** @xstate-layout N4IgpgJg5mDOIC5QHcCGAbA1gOjQSwBc8A7KAYnTFQDcwACACwHsBbMRUABydkLyeIcQAD0QBaAIwAWAKzYADAHYAHBImKATBPkA2RTICcUgDQgAnog0bsOgMwqDuqfPkGDGzQF9PptFmwCdKh0fphkMAR0AEaoAMaYjKzsSCDcvEQCQqIIkjI62BIGOsoymrYaBrYyEqYWCBryEtgGMkpt6lYGEsrePiDETBBwQqG4qHykQml8mSnZYra28gV6yq46Og0bOrWIEvkS9srHS7ZSjrbu3r4YOIHBoVM8M4JziLbYyiqyis6KZR4pIpdggJBoPjIpM55DJLgZ-osJNcQKNQnQwMQhhAnul+K9QPMzlJsJCNMoNFJCrCpLZlCClstzoZYYodJCZF9kY8UtMMviROJbMUSVIyRSqWdaSCxHlPpttBUtLC1LZep4gA */
@@ -72,7 +72,7 @@ const machine =
   });
 
 const machine2 =
-  /** @xstate-layout N4IgpgJg5mDOIC5QGECGAbdACAtqgxgBYCWAdmAHQCyx+ATgPYAOhD51ArgC6QDEAqqRzcwiUEwaxiXYmzEgAHogC0AJgDMARgqaALAFZ1ADnUBOAAxGAbLqMAaEAE8VR7QHZTR-W+tXN5qw8AXyCHNExcAhJ2GnpmVnZBYR4IXioReQkpGTkkRRV1VTcKXTcPfVNVU09DUodnBGVXCg9rczLPI1UbELCMbDwiMkoANWIIMAYKAGVWAHdSMigsADdxyd4ACXXV9YZMyWlZUnklBHUbClVzItN1A28Hq3qXdxqfKz8A4NCQcIGosMKGMJlNthAlrtQbxZgw5lDJgdssdTohNGUWuZvFo7pVurpdC9Gs1Wt5fP5AqYQr9SAwJvA8v9IkMYrRGCw2JR0ikkUdcqAzmpSjo3J91PcrNcjG51EShUYKF03N8ulZTLoqr0-v1mdEuWz4pyKEkRBBeTkTnlBap9FYKFYyYEtNLPLKnIhpS1PKYfK6jCYsVqmYM9dQDRzyOaUVaCvptKVypVql57m45YVtErzAYrOZ1KLXG4gzqQ0CQZMZvNFqRlmtQVH+fkEBoFaobVpPpKyupLHLVLZFUUsbp89LdIFixFS+xy2DxpC64i8lk+ZaBYge8VvIZlUYCaYHTKiZ7Wj6jH6A6pJwCWaM9g2103NIZMdjNLi2zZCe7GhnB8qM30IC3GuKxr11YYH1RX83H0EoOgqKoalTdN1X-LEblFGoqWpIA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QGECGAbdACAtqgxgBYCWAdmAHQCyx+ATgPYAOhD51ArgC6QDEAqqRzcwiUEwaxiXYmzEgAHogC0AJgCcANgqbVAZgAsAdk3qAHKoCsmzXoA0IAJ4rLlinoAMqswYCMRvV9zXz1NAF8whzRMXAISdhp6ZlZ2QWEeCF4qEXkJKRk5JEUVPXVVCnVrD0t9AKMjfwMHZwRlV3cvMyMPM1MA80sIqIxsPCIySgA1YggwBgoAZVYAd1IyKCwANxm53gAJHa2dhlzJaVlSeSUEDTMKI1VjDw89M18LAOaXN09vPwCgm9QkMQNFRnEJhRprN5gcIOsjjDeEsGMtEXNTvkLldEL5fOVVKoTK9epZPG8zF9Wu1fj5-IFgsCQaQGLN4EUwbFxglaIwWGxKNkMpjzoVQNdlIFyppLMZVDLQs9XEYqWpCe5AnpyeojGZrEzhjExvFBbzkgKKGkRBARQVLkUJXp1SFLFoXmUGjYqXiDBQNMZfK78VotCDOcbIYk+SlREU8qL7eKVH5tJVNNVanp6o1VRofoHuvT1B4DOYwyMuSaocdFis1qQNtsYbbsQ7cQ97nL9JYzL3ZZSnCpCeUtSFGqWbB5fOFIqCKxH2NC5hQ4Qimxi42c7TibgZyh4dUT6g0S0YDJpvb5ff6GkGNKZrOWjRDF8cW2Lighe36ibZewqeneXNKncQNNF7DQejJGdDXBblY3ELdWyTVoDAMDwKiqGosyzBoz1VUI7k8Z4Hh7LwDCdAwIgiIA */
   createMachine({
     type: "parallel",
     states: {
